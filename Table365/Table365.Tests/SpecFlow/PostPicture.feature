24383521e6b4c13f2cddfb8af05cc0980026a7a9
Kenneth Tsai <bustta80980@gmail.com>
2016-08-10 11:07:08 +0100

    init feature and step files for BDD

---
+++ Table365/Table365.Tests/SpecFlow/PostPicture.feature
@@ -0,0 +1,8 @@
+﻿Feature: PostPicture
+	ensure the photo be saved sucessfully
+
+@mytag
+Scenario: Add two numbers
+	Given I have received a photo from client side
+	When save into DB
+	Then the count of table Photo should be plus one
---
+++ Table365/Table365.Tests/SpecFlow/PostPictureSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Table365.Tests.SpecFlow
+{
+    [Binding]
+    public class PostPictureSteps
+    {
+        [Given(@"I have received a photo from client side")]
+        public void GivenIHaveReceivedAPhotoFromClientSide()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"save into DB")]
+        public void WhenSaveIntoDb()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the count of table Photo should be plus one")]
+        public void ThenTheCountOfTablePhotoShouldBePlusOne()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
